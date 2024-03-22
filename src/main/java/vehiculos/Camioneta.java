ce7d18a18b3a1ca3d72403941d329ed94290f404
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-22 12:57:18 -0500

    Clase Camioneta

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -1,5 +1,38 @@
 package vehiculos;
 
-public class Camioneta {
+
+public class Camioneta extends Vehiculo {
+	private boolean volco;
+	private static int CantidadVehiculos;
+	
+	public Camioneta() {
+		
+	}
+	// Constructor
+	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
+		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
+		this.volco = volco;
+		CantidadVehiculos++;
+	}
+	
+	// Métodos 
+
+	
+	// Métodos get y set
+    public boolean isVolco() {
+        return volco;
+    }
+
+    public void setVolco(boolean volco) {
+        this.volco = volco;
+    }
+
+    public static int getCantidadVehiculos() {
+        return CantidadVehiculos;
+    }
+
+    public static void setCantidadVehiculos(int cantidadVehiculos) {
+        CantidadVehiculos = cantidadVehiculos;
+    }
 
 }
