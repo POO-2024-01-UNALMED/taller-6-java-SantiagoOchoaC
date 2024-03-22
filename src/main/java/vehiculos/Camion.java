7f22ca92598ae5efdc5e2d094da5a4d7e604941c
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-22 12:56:20 -0500

    Clase Camion.

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -1,5 +1,37 @@
 package vehiculos;
 
-public class Camion {
+public class Camion extends Vehiculo {
+	private int ejes;
+	private static int CantidadVehiculos;
+	
+	public Camion() {
+		
+	}
+	// Constructor
+	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
+		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
+		this.ejes = ejes;
+		CantidadVehiculos++;
+	}
+	
+	
+	
+	
+	
+	// Métodos get y set
+    public int getEjes() {
+        return ejes;
+    }
 
+    public void setEjes(int ejes) {
+        this.ejes = ejes;
+    }
+
+    public static int getCantidadVehiculos() {
+        return CantidadVehiculos;
+    }
+
+    public static void setCantidadVehiculos(int cantidadVehiculos) {
+        CantidadVehiculos = cantidadVehiculos;
+    }
 }
